8e8aa6f8f47929a4ee3fb44763f335ab92524b73
JoseWilian <josewilian.work@gmail.com>
2025-02-14 12:05:44 -0300

    :construction: view to register books

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books/create', function () {
+    return view('book_store.pages.books.create');
+});
